a99ad6a0da0962a0a1c451cb90f5bff4246a8b7a
Alex-mumo <alexymumo2000@gmail.com>
2022-02-23 23:37:57 +0300

    added navigation graph

--- app/src/main/java/com/example/todoapp/NoteApplication.kt
+++ app/src/main/java/com/example/todoapp/NoteApplication.kt
@@ -1,6 +1,13 @@
 package com.example.todoapp
 
 import android.app.Application
+import timber.log.Timber
 
 class NoteApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
