5ab2a4a1e58fc7c1d015f28dd987a2331f912f9d
Alex-mumo <alexymumo2000@gmail.com>
2022-02-24 18:22:14 +0300

    added recyclerview

--- app/src/main/java/com/example/todoapp/ui/adapters/NoteRecyclerView.kt
+++ app/src/main/java/com/example/todoapp/ui/adapters/NoteRecyclerView.kt
@@ -1,4 +1,45 @@
 package com.example.todoapp.ui.adapters
 
-class NoteRecyclerView {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.R
+import com.example.todoapp.data.entity.Note
+
+class NoteRecyclerView: ListAdapter<Note, NoteRecyclerViewHolder>(NoteComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteRecyclerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+
+        return NoteRecyclerViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: NoteRecyclerViewHolder, position: Int) {
+        val note = getItem(position)
+        holder.title.text = note.title
+        holder.description.text = note.description
+    }
+}
+
+class NoteRecyclerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val title: TextView = itemView.findViewById(R.id.title)
+    val description: TextView = itemView.findViewById(R.id.description)
+
+
+}
+
+class NoteComparator: DiffUtil.ItemCallback<Note>() {
+    override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem == newItem
+    }
+
 }
